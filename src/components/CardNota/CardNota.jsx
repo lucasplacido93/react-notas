feb17cdecee40701f59279f96cf3efe4ceb103cc
Lucas Reis <lucas.reis@capgemini.com>
2021-03-04 15:35:08 -0300

    feat: adicionado index.js e estilização

--- src/components/CardNota/CardNota.jsx
+++ src/components/CardNota/CardNota.jsx
@@ -1,14 +1,14 @@
 import React, { Component } from "react";
-import "./estilo.css"
+import "./estilo.css";
 
 class CardNota extends Component {
   render() {
     return (
-      <section className = "card-nota">
-        <header className = "card-nota-cabecalho">
-          <h3 className = "card-nota-titulo" >Título</h3>
+      <section className="card-nota">
+        <header className="card-nota_cabecalho">
+          <h3 className="card-nota_titulo">Título</h3>
         </header>
-        <p className = "card-nota-texto">Escreva sua nota</p>
+        <p className="card-nota_texto">Escreva sua nota</p>
       </section>
     );
   }
